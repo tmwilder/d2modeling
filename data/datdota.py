dc2f87b8f2bb2ade0eaacb2adc1bb909b3aec3a1
Tim Wilder <tmwilder@gmail.com>
2014-05-28 21:58:12 -0700

    Added upcoming match logic
    Still need to add saving

--- data/datdota.py
+++ data/datdota.py
@@ -2,57 +2,91 @@ import requests
 import bs4
 
 
-def get_data_page(records_back=0):
+def _get_past_matches_page(records_back=0):
     params = {"l0": records_back}
-    url = 'http://www.datdota.com/matches.php'
+    url = "http://www.datdota.com/matches.php"
     res = requests.get(url, params=params)
     return res
 
 
-def parse_results_from_page(text):
+def _parse_results_from_past_matches(text):
     doc = bs4.BeautifulSoup(text)
     table = doc.find_all("table")[0]
 
-    rows = table.find_all('tr')
+    rows = table.find_all("tr")
 
     parsed_headers = [header.text.lower() for header in rows[0]]
 
     results = []
 
     for row in rows[1:]:
-        data = row.find_all('td')
+        data = row.find_all("td")
         parsed_data = [datum.text for datum in data]
         
         formatted_data = dict(zip(parsed_headers, parsed_data))
 
-
         results.append(formatted_data)
 
-    set_team_scores(results)
+    _set_team_scores(results)
 
     return results
 
 
-def set_team_scores(results):
+def _set_team_scores(results):
     for datum in results:
-        score = datum.pop(u'score')
-        radiant_score, dire_score = [int(team_score.strip()) for team_score in score.split('-')]
-        datum[u'radiant_score'] = radiant_score
-        datum[u'dire_score'] = dire_score
+        score = datum.pop("score")
+        radiant_score, dire_score = [int(team_score.strip()) for team_score in score.split("-")]
+        datum[u"radiant_score"] = radiant_score
+        datum[u"dire_score"] = dire_score
 
 
 def save_data(data):
     pass
 
 
-def main():
+def _get_upcoming_matches_page():
+    url = "http://www.datdota.com/upcoming_matches.php"
+    res = requests.get(url=url)
+    return res
+
+
+def _parse_results_from_upcoming_matches(text):
+    # TODO factor parsing methods into common class if we do one more of these.
+    doc = bs4.BeautifulSoup(text)
+    table = doc.find_all("table")[0]
+
+    rows = table.find_all("tr")
+
+    parsed_headers = [header.text.lower() for header in rows[0]]
+
+    results = []
+
+    for row in rows[1:]:
+        data = row.find_all("td")
+        parsed_data = [datum.text for datum in data]
+        formatted_data = dict(zip(parsed_headers, parsed_data))
+        results.append(formatted_data)
+
+    return results
+
+
+def get_past_matches(how_many_500_matches):
     result = []
-    for n in range(5):
-        data_page = get_data_page(500*n)
-        data = parse_results_from_page(data_page.text)
+    for n in range(how_many_500_matches):
+        data_page = _get_past_matches_page(500*n)
+        data = _parse_results_from_past_matches(data_page.text)
         result.extend(data)
-    save_data(result)
+
+
+def get_upcoming_matches():
+    upcoming_page = _get_upcoming_matches_page()
+    results = _parse_results_from_upcoming_matches(upcoming_page.text)
+    return results
 
 
 if __name__ == "__main__":
-    main()
+    past_matches = get_past_matches(1)
+    save_data(past_matches)
+
+    upcoming_matches = get_upcoming_matches()
+    save_data(upcoming_matches)
